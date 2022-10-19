d08122891d7fc480eb27242b0c65a1f4d36aa331
reslear <reslear@gmail.com>
2022-10-19 18:47:49 +0200

    refactor: add fully typed `Lottie` interfaces

--- src/types.ts
+++ src/types.ts
@@ -14,10 +14,45 @@ export interface DotLottieManifest {
 }
 
 export interface LottieJsonAsset {
-  p: string
-  e: number
+  id: string
+  u?: string
+  p?: string
+  e?: number
+  layers?: LottieJsonLayer[]
+}
+
+export interface LottieJsonLayer {
+  ty: number
+  nm: string
+  ks: any
+  ao: number
+  ddd: number
+  ind: number
+  ip: number
+  op: number
+  refId?: string
 }
 
 export interface LottieJson {
+  // Version
+  v: string
+  // Frame Rate
+  fr: number
+  // Start keyframe
+  ip: number
+  // End keyframe
+  op: number
+  // Width
+  w: number
+  // Height
+  h: number
+  // Duration
+  nm: string
+  // 3d
+  ddd: number
+  // Assets
   assets: LottieJsonAsset[]
+  // Layers
+  layers: LottieJsonLayer[]
+  markers: LottieJsonLayer[]
 }
