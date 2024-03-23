09ce502bb21519acccf224d00cc223d99d494e10
deathcrafter <sahooshaktijeet@gmail.com>
2024-03-24 01:40:11 +0530

    upgraded tsup to generate .d.mts files for better TypeScript/ESM support

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'tsup'
+import { copyFileSync } from 'fs'
 
-export default defineConfig((options) => {
-  return {
-    format: ['cjs', 'esm'],
-    dts: true,
-    minify: false,
-    entry: ['src/index.ts'],
-    splitting: false,
-    sourcemap: true,
-    clean: true,
-  }
+export default defineConfig({
+  format: ['cjs', 'esm'],
+  dts: true,
+  minify: false,
+  entry: ['src/index.ts'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
 })
